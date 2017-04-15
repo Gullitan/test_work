fb3f1382b7e844957c1d843f39e705745da7cbae
Gullitan <1994slavaslava@gmail.com>
2017-04-15 15:21:56 +0300

    commit

---
+++ source-js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').fadeIn(250);
+        },
+        function(){
+            $(this).find('.caption').fadeOut(250);
+        }
+    );
+});
+
+
